56f44956380e16966fbcbb180d8eafe705aaeeeb
Chris Pollock <73022650+ChristopherPollock@users.noreply.github.com>
2022-10-17 00:35:44 -0400

    Module 3.5 Complete Advanced network changes

--- networks.tf
+++ networks.tf
@@ -30,6 +30,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+
 #create IGW in us-west 2-1
 resource "aws_internet_gateway" "igw-oregon" {
   provider = aws.region-worker
@@ -45,7 +46,7 @@ data "aws_availability_zones" "azs" {
   state    = "available"
 }
 
-#create subnet in #1 unb us-east-1
+#create subnet #1  us-east-1
 resource "aws_subnet" "subnet-1" {
   provider          = aws.region-master
   availability_zone = element(data.aws_availability_zones.azs.names, 0)
@@ -56,7 +57,18 @@ resource "aws_subnet" "subnet-1" {
   }
 }
 
-#create subnet in #1 unb us-west-2
+#create subnet #2 us-east-1
+resource "aws_subnet" "subnet-2" {
+  provider          = aws.region-master
+  availability_zone = element(data.aws_availability_zones.azs.names, 1)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Env = "Terraform Lab"
+  }
+}
+
+#create subnet us-west-2
 resource "aws_subnet" "subnet-1-oregon" {
   provider   = aws.region-worker
   vpc_id     = aws_vpc.vpc_oregon.id
@@ -64,4 +76,80 @@ resource "aws_subnet" "subnet-1-oregon" {
   tags = {
     Env = "Terraform Lab"
   }
+}
+
+#initiate Peering connection request from us-east-1
+resource "aws_vpc_peering_connection" "vpc-useast1-uswest2" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_oregon.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+  tags = {
+    Env = "Terraform Lab"
+  }
+}
+
+#Accept VPC Peering connection request in us-west-2 from us-east-1
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc-useast1-uswest2.id
+  auto_accept               = true
+  tags = {
+    Env = "Terraform Lab"
+  }
+}
+
+#Create router table in us-east-1
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block                = "192.168.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc-useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+    Env  = "Terraform Lab"
+  }
+}
+#Overwrite the default route table of the VPC (Master) with our route table entries
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+#Create router table in us-west-2
+resource "aws_route_table" "internet_route_oregon" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_oregon.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-oregon.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc-useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+    Env  = "Terraform Lab"
+  }
+}
+
+#Overwrite the default route table of the VPC (Master) with our route table entries
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-worker
+  vpc_id         = aws_vpc.vpc_oregon.id
+  route_table_id = aws_route_table.internet_route_oregon.id
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+Terraform Lab file.
+
+Lesson 3.5 module image:
+![][def]
+
+[def]: images/lesson3.5objective.png
